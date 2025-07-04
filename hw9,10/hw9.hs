4a7ded9e77d1a8bb360adb4186909b82a35bda97
Bourbaki0404 <vector_space@foxmail.com>
2025-07-04 21:54:40 +0800

    update

--- hw9,10/hw9.hs
+++ hw9,10/hw9.hs
@@ -1,1 +1,8 @@
+-- Funny takes two arguments, the first one a type of kind * -> *, 
+-- and the second a type of kind *, and constructs a type. 
+-- (How did GHCi know what the kind of Funny is? Well, 
+-- it does kind inference just like it also does type inference.)
+-- Funny is a higher-order type constructor, 
+-- in the same way that map is a higher-order function. 
+-- Note that types can be partially applied, too, just like functions:
 data Funny f a = Funny a (f a)
